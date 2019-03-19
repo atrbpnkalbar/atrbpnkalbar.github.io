30d6a541df8adfd8ae4aa6a9bf6b1a8636898f6c
atrbpnkalbar <atr.bpn.kalbar@gmail.com>
2019-03-19 14:15:23 +0700

    update pontianak

---
+++ pontianak/data/SurveiLapangan_6.js
@@ -0,0 +1,8 @@
+var json_SurveiLapangan_6 = {
+"type": "FeatureCollection",
+"name": "SurveiLapangan_6",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "F_CIL": 1.0, "Nomer_BLAD": "4524-8687", "Desa_atau": "Pontianak", "Kecamatan": "Pontianak", "Kabupaten": "Pontianak", "Tanggal_In": "2019-03-19", "Koordinat": 109.3219, "Koordina_1": -0.04436, "Nama_Petug": "andi", "Nama_Pet_1": "ari", "Nama_Pet_2": "yogi", "Nama_Pet_3": "sigit", "Keterangan": "Test aja", "F__Q_Name1": "Tegalan", "J_Tanam": null, "S_Irigasi": "Irigasi", "N_Irigasi": 1.0, "INT_Tanam": "Sawah 2x Panen atau Lebih", "N_Intensit": 4.0, "Reklas_CH": "Tinggi>2500", "N_CH": 3.0, "Lereng": "0-2%", "N_Lereng": 2.0, "Tekstur": "Sedang", "N_Tekstur": 2.0, "Bencana": "Kawasan rawan bencana geologi", "N_Bencana": 2.0, "Skoring": null, "Kelas": "Kelas 3", "RTRW": "Jasa", "F_RTRW": "Fungsi Lindung", "Kawasan_Hu": "Hutan Suaka Alam dan Wisata", "F_Hutan": "Fungsi Lindung", "Izin_PTP": "PTP", "F_Izin_PTP": "Tidak Ada Perizinan\/PTP", "F_Izin_P_1": "Tidak Ada Perizinan\/PTP", "RPS": "Jalur KCIC", "F_Rencana": "Tidak Ada Rencana Strategis", "GUPT": "Hak Milik", "F_GUPT": "Selain HGU dan\/atau HGB", "Usulan": "3_Direkomendasikan Bersama)", "Gambar": "<img src=' https:\/\/2.bp.blogspot.com\/-W5Z-yg0NL7U\/XJCSFUlTplI\/AAAAAAAAAGU\/e0cGRd-s7dUWaaBwwPaPhABFCzwuZzxqgCLcBGAs\/s1600\/ribuan-hektare-sawah-di-pangandaran-terancam-kekeringan-N1M_resize.jpg '>" }, "geometry": { "type": "Point", "coordinates": [ 109.3219, -0.04436 ] } }
+]
+}
