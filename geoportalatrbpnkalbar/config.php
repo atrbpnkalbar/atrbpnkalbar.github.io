50a74bc38c88321ee3cf830d9b3ccabed2d86c54
atrbpnkalbar <atr.bpn.kalbar@gmail.com>
2019-03-21 11:03:07 +0700

    change data

--- geoportalatrbpnkalbar/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-//include dirname(__FILE__) . '/../config.php';
-
-$config["server"]='localhost';
-$config["username"]='root';
-$config["password"]='';
-$config["database_name"]='gisel';
--- geoportalatrbpnkalbar/home.php
+++
@@ -1,6 +0,0 @@
-<div class="page-header">
-    <h1></h1>
-</div>
-<body>
-<img src="" align="right" />
-</body>
